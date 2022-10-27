498bfc8a57aa3999e7dea932ce24e984f46f427d
heitormsb <heitormsb@gmail.com>
2022-10-27 20:50:39 -0300

    add info EJs

--- main.py
+++ main.py
@@ -190,16 +190,21 @@ Obs: Caso seje de algumas dessa(s) atletica(s) (ou outras da FGA) mande para @he
 async def ej(message: types.Message):
     textoEJ = """
     *EMPRESAS JUNIORES:*
+- As Empresas juniores tem o propósito de formar líderes comprometidos e capazes para o mercado
+- Todas as EJs aceitam os 5 cursos da FGA
 
-Zenit Aerospace
+*Zenit Aerospace*
 
-Orc'estra Gamificação
+*Orc'estra Gamificação*
+Empresa Júnior de Engenharia de Software. Tem o propósito em trazer soluções de software e gamificação para o mercado. 
+Saiba mais em: [@orcgamificacao](https://www.instagram.com/orcgamificacao/) no instagram.
 
-Matriz Engenharia De Energia
 
-Engrena
+*Matriz Engenharia De Energia*
 
-Eletronjun 
+*Engrena*
+
+*Eletronjun*
 
 Obs: Caso seje de algumas dessas EJs (ou outras da FGA) mande para @heitormsb uma pequena descrição/introdução da sua EJ e forma de contato, para colocar aqui. Obrigado.
    """
