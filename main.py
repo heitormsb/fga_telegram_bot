15611afef4113a865b0a9a03810ea4cab1da0e38
heitormsb <heitormsb@gmail.com>
2022-12-06 20:12:22 -0300

    add: default commands

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ from handlers.atletica.atletica import register_handlers_atletica
 from handlers.ej.ej import register_handlers_ej
 from handlers.competicao.competicao import register_handlers_competicao
 
+from handlers.eventos.eventos import register_handlers_eventos
+
 from env import API_TOKEN
 
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ParseMode
+from aiogram.types import ParseMode, BotCommand
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 
 
 # Configure logging
@@ -28,7 +31,21 @@ storage = MemoryStorage()
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
-
+    
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands([
+        BotCommand("ajuda", "Mostra os comandos disponíveis"),
+        BotCommand("grupos", "Link grupos Engenharias"),
+        BotCommand("fluxos", "Fluxogramas das Engenharias"),
+        BotCommand("estagio", "Estágio"),
+        BotCommand("secretaria", "Contatos da secretaria"),
+        BotCommand("coordenadores", "Coordenadores das Engenharias"),
+        BotCommand("calendario", "Calendário acadêmico"),
+        BotCommand("creditos", "Aproveitamento de créditos por atividade complementar"),
+        BotCommand("atleticas", "Atleticas"),
+        BotCommand("ej", "Empresas juniores"),
+        BotCommand("competicao", "Equipes de competição"),
+    ])
 
 # Comando de menu de ajuda inicial | comandos: 'ajuda', 'menu', 'help', 'start'
 register_handlers_ajuda(dp)
@@ -66,11 +83,15 @@ register_handlers_competicao(dp)
 # Comando ouvidoria | comandos: 'ouvidoria'
 register_handlers_ouvidoria(dp)
 
+# Comando eventos | comandos: 'eventos', 'evento'
+register_handlers_eventos(dp) 
+
 # Apagar comandos invalidos do chat
 @dp.message_handler()
 async def invalidCommand(message: types.Message):
     if message.text[0] == '/' and not " " in message.text:
         await bot.delete_message(message.chat.id, message.message_id)  
 
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=set_default_commands)
